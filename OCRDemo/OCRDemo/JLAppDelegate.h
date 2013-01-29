db747cccc9b874f11901293bf18483e725141321
richieliuse <richieliuse@gmail.com>
2013-01-29 22:58:18 +0800

    1. start the project

---
+++ OCRDemo/OCRDemo/JLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLAppDelegate.h
+//  OCRDemo
+//
+//  Created by Richie Liu on 13-1-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OCRDemo/OCRDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCRDemo
+//
+//  Created by Richie Liu on 13-1-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLAppDelegate class]));
+    }
+}
